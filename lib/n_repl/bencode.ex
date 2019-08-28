8834a1a84d610bb85a8aa0d39b74095c5ede2d78
Ben Damman <ben.damman@gmail.com>
2019-08-27 23:47:00 -0700

    add doc

--- lib/n_repl/bencode.ex
+++ lib/n_repl/bencode.ex
@@ -1,6 +1,9 @@
 defmodule NRepl.Bencode do
   import Bento
 
+  @@doc """
+  Bencodes message struct after removing empty keys and normalizing key names.
+  """
   def encode(message) do
     if NRepl.Message.valid?(message) do
       Map.from_struct(message)
